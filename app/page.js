40b024f70e2d57501b1c42bd0118e5b7cf68d5fe
Gulshan <shrigulshankumar@gmail.com>
2024-10-13 01:42:21 +0300

    Added initial setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="d-flex justify-content-center  align-items-center vh-100">Hello</h1>
+   </div>
+  );
+}
