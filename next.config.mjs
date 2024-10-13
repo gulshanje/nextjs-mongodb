b478dba4ef7809ae5dca0f6276465532888e38eb
Gulshan <shrigulshankumar@gmail.com>
2024-10-13 12:17:38 +0300

    Added mongoose environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import { DB_URI } from './config';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DB_URI: config.DB_URI
+    }
+};
 
 export default nextConfig;
